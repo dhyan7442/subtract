9ae4d3154d5ba7bf87208bdaec7325d3fc181621
dhyan7442 <djxtreme7442@gmail.com>
2025-01-27 15:01:55 +0530

    Update and rename README.md to sub.java
    java program

---
+++ sub.java
@@ -0,0 +1,12 @@
+class sub
+{
+  public static void main(String args[])
+  {
+    int a,b,sub;
+    a=9;
+    b=4;
+    sub=a-b;
+    System.out.println("subtraction of 2 numbers is"+sub);
+  }
+}
+  
